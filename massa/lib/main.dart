92ff50c65b599c19122f705b472d9d0e71b34689
Marco <marco.merini@contaazul.com>
2023-12-13 12:33:04 -0300

    main.dart e pubspec.yaml

--- massa/lib/main.dart
+++ massa/lib/main.dart
@@ -1,125 +1,235 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+class Person {
+  String name;
+  double height;
+  double weight;
+
+  Person({required this.name, required this.height, required this.weight});
+
+  double calculateBMI() {
+    return weight / (height * height);
+  }
+}
+
+class PersonList with ChangeNotifier {
+  List<Person> _persons = [];
+
+  List<Person> get persons => _persons;
+
+  void addPerson(Person person) {
+    _persons.add(person);
+    notifyListeners();
+  }
+
+  void editPerson(int index, Person person) {
+    _persons[index] = person;
+    notifyListeners();
+  }
+
+  void deletePerson(int index) {
+    _persons.removeAt(index);
+    notifyListeners();
+  }
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: ChangeNotifierProvider(
+        create: (context) => PersonList(),
+        child: PersonListView(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class PersonListView extends StatelessWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('IMC Calculator'),
+      ),
+      body: Consumer<PersonList>(
+        builder: (context, personList, child) {
+          return ListView.builder(
+            itemCount: personList.persons.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(personList.persons[index].name),
+                subtitle: Text(
+                    'BMI: ${personList.persons[index].calculateBMI().toStringAsFixed(2)}'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => PersonDetailsView(index),
+                    ),
+                  );
+                },
+                onLongPress: () {
+                  personList.deletePerson(index);
+                },
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => PersonAddView(),
+            ),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class PersonDetailsView extends StatefulWidget {
+  final int index;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  PersonDetailsView(this.index);
+
+  @override
+  _PersonDetailsViewState createState() => _PersonDetailsViewState();
+}
+
+class _PersonDetailsViewState extends State<PersonDetailsView> {
+  final _nameController = TextEditingController();
+  final _heightController = TextEditingController();
+  final _weightController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    final person =
+        Provider.of<PersonList>(context, listen: false).persons[widget.index];
+    _nameController.text = person.name;
+    _heightController.text = person.height.toString();
+    _weightController.text = person.weight.toString();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Edit Person'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(labelText: 'Name'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            TextField(
+              controller: _heightController,
+              decoration: InputDecoration(labelText: 'Height'),
+              keyboardType: TextInputType.number,
+            ),
+            TextField(
+              controller: _weightController,
+              decoration: InputDecoration(labelText: 'Weight'),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 16),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    final name = _nameController.text;
+                    final height = double.parse(_heightController.text);
+                    final weight = double.parse(_weightController.text);
+                    final person =
+                        Person(name: name, height: height, weight: weight);
+
+                    Provider.of<PersonList>(context, listen: false)
+                        .editPerson(widget.index, person);
+                    Navigator.pop(context);
+                  },
+                  child: Text('Save'),
+                ),
+                SizedBox(width: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Cancel'),
+                ),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class PersonAddView extends StatefulWidget {
+  @override
+  _PersonAddViewState createState() => _PersonAddViewState();
+}
+
+class _PersonAddViewState extends State<PersonAddView> {
+  final _nameController = TextEditingController();
+  final _heightController = TextEditingController();
+  final _weightController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Person'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(labelText: 'Name'),
+            ),
+            TextField(
+              controller: _heightController,
+              decoration: InputDecoration(labelText: 'Height'),
+              keyboardType: TextInputType.number,
+            ),
+            TextField(
+              controller: _weightController,
+              decoration: InputDecoration(labelText: 'Weight'),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                final name = _nameController.text;
+                final height = double.parse(_heightController.text);
+                final weight = double.parse(_weightController.text);
+                final person =
+                    Person(name: name, height: height, weight: weight);
+
+                Provider.of<PersonList>(context, listen: false)
+                    .addPerson(person);
+                Navigator.pop(context);
+              },
+              child: Text('Add Person'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
